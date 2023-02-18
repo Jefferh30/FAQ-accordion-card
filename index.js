8ad07f82bcbe1dc6ae838aa8678cd5406b31ca68
Jefferson Huera <jefferh30@gmail.com>
2023-02-17 23:08:48 -0300

    Rotation to arrows in questions added

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const questionHeadings = document.querySelectorAll('.question-heading');
 
 questionHeadings.forEach((heading) => {
   heading.addEventListener('click', () => {
+    //toggle the display of all other elements to none
     questionHeadings.forEach(element => {
       element.nextElementSibling.style.display = "none";
     });
+
+    //set all arrows to default position
+    questionHeadings.forEach(element => {
+      element.children[1].style.transform = "none";
+    });
+
     // get the content element for this question heading
     const content = heading.nextElementSibling;
 
-    // array of question headinds minus current one
-    
-    const arrow = content.lastChild;
+    //get the arrow and rotate it
+    const arrow = heading.children[1];
+    arrow.style.transform = "rotate(180deg)";
     
     // toggle the display property of the content element
     if (content.style.display === "block") {
