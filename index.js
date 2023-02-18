fc87bd6812f2502d5fc8186ec1b57ff1337ac75c
Jefferson Huera <jefferh30@gmail.com>
2023-02-17 22:50:37 -0300

    Toggle elements improved

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const questionHeadings = document.querySelectorAll('.question-heading');
 
 questionHeadings.forEach((heading) => {
   heading.addEventListener('click', () => {
+    questionHeadings.forEach(element => {
+      element.nextElementSibling.style.display = "none";
+    });
     // get the content element for this question heading
     const content = heading.nextElementSibling;
+
+    // array of question headinds minus current one
+    
+    const arrow = content.lastChild;
     
     // toggle the display property of the content element
     if (content.style.display === "block") {
@@ -14,3 +21,4 @@ questionHeadings.forEach((heading) => {
     }
   });
 });
+
